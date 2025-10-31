b8b062dcb4ccfac68a75a20d1ce9568e6fe103ec
Alex Reyes <alex.reyes.o@mail.pucv.cl>
2025-10-31 19:09:00 -0300

    Update Ball2.java
    Modificar el comportamiento de los meteoritos para que caigan

--- core/src/main/java/puppy/code/Ball2.java
+++ core/src/main/java/puppy/code/Ball2.java
@@ -6,75 +6,77 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
-
 public class Ball2 {
-	private int x;
-    private int y;
+    private float x;
+    private float y;
     private int xSpeed;
     private int ySpeed;
     private Sprite spr;
+    private boolean active = true;
 
-    public Ball2(int x, int y, int size, int xSpeed, int ySpeed, Texture tx) {
-    	spr = new Sprite(tx);
-    	this.x = x; 
- 	
-        //validar que borde de esfera no quede fuera
-    	if (x-size < 0) this.x = x+size;
-    	if (x+size > Gdx.graphics.getWidth())this.x = x-size;
-         
+    public Ball2(float x, float y, int size, int xSpeed, int ySpeed, Texture tx) {
+        spr = new Sprite(tx);
+        this.x = x;
         this.y = y;
-        //validar que borde de esfera no quede fuera
-    	if (y-size < 0) this.y = y+size;
-    	if (y+size > Gdx.graphics.getHeight())this.y = y-size;
-    	
+        
+        // AJUSTAR TAMA;OS SPRITES
+        spr.setSize(size, size);
         spr.setPosition(x, y);
-        this.setXSpeed(xSpeed);
-        this.setySpeed(ySpeed);
+        
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
     }
+    
     public void update() {
-        x += getXSpeed();
-        y += getySpeed();
+        // MOVIMIENTO CON CAÍDA
+        x += xSpeed;
+        y += ySpeed; 
 
-        if (x+getXSpeed() < 0 || x+getXSpeed()+spr.getWidth() > Gdx.graphics.getWidth())
-        	setXSpeed(getXSpeed() * -1);
-        if (y+getySpeed() < 0 || y+getySpeed()+spr.getHeight() > Gdx.graphics.getHeight())
-        	setySpeed(getySpeed() * -1);
+        // ELIMINAR CUANDO SE SALGA
+        if (y + spr.getHeight() < 0) {
+            active = false;
+        }
+        
+        // REBOTE EN LOS LADOS
+        if (x < 0 || x + spr.getWidth() > Gdx.graphics.getWidth()) {
+            xSpeed = -xSpeed;
+        }
+        
         spr.setPosition(x, y);
+    }
+    
+    public boolean isActive() {
+        return active;
     }
     
     public Rectangle getArea() {
-    	return spr.getBoundingRectangle();
+        return spr.getBoundingRectangle();
     }
+    
     public void draw(SpriteBatch batch) {
-    	spr.draw(batch);
+        spr.draw(batch);
     }
     
     public void checkCollision(Ball2 b2) {
-        if(spr.getBoundingRectangle().overlaps(b2.spr.getBoundingRectangle())){
-        	// rebote
-            if (getXSpeed() ==0) setXSpeed(getXSpeed() + b2.getXSpeed()/2);
-            if (b2.getXSpeed() ==0) b2.setXSpeed(b2.getXSpeed() + getXSpeed()/2);
-        	setXSpeed(- getXSpeed());
-            b2.setXSpeed(-b2.getXSpeed());
-            
-            if (getySpeed() ==0) setySpeed(getySpeed() + b2.getySpeed()/2);
-            if (b2.getySpeed() ==0) b2.setySpeed(b2.getySpeed() + getySpeed()/2);
-            setySpeed(- getySpeed());
-            b2.setySpeed(- b2.getySpeed()); 
+        if (active && b2.active && spr.getBoundingRectangle().overlaps(b2.spr.getBoundingRectangle())) {
+            xSpeed = -xSpeed;
+            b2.xSpeed = -b2.xSpeed;
         }
     }
-	public int getXSpeed() {
-		return xSpeed;
-	}
-	public void setXSpeed(int xSpeed) {
-		this.xSpeed = xSpeed;
-	}
-	public int getySpeed() {
-		return ySpeed;
-	}
-	public void setySpeed(int ySpeed) {
-		this.ySpeed = ySpeed;
-	}
-	
     
+    public int getXSpeed() {
+        return xSpeed;
+    }
+    
+    public void setXSpeed(int xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+    
+    public int getySpeed() {
+        return ySpeed;
+    }
+    
+    public void setySpeed(int ySpeed) {
+        this.ySpeed = ySpeed;
+    }
 }
