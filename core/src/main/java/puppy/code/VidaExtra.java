70339d38a8686572562c40781513b294462d4c01
Alex Reyes <alex.reyes.o@mail.pucv.cl>
2025-10-31 21:08:49 -0300

    Update VidaExtra.java
    Arreglo para funcionar con colisionable

--- core/src/main/java/puppy/code/VidaExtra.java
+++ core/src/main/java/puppy/code/VidaExtra.java
@@ -3,9 +3,28 @@ package puppy.code;
 import com.badlogic.gdx.graphics.Texture;
 
 public class VidaExtra extends GameObject implements Colisionable {
+    private boolean efectoActivado = false;
+    private int tiempoEfecto = 0;
     
     public VidaExtra(float x, float y, Texture tx) {
-        super(x, y, 30, 0, -2, tx); // BAJA MAS LENTO
+        super(x, y, 30, 0, -2, tx);
+    }
+    
+    @Override
+    public void update() {
+        if (!efectoActivado) {
+            super.update();
+        } else {
+            //EFECTO ESPECIAL
+            tiempoEfecto++;
+            spr.setScale(1.0f + (tiempoEfecto * 0.1f)); //SE AGRANDA
+            spr.setAlpha(1.0f - (tiempoEfecto * 0.05f)); // SE DESAPARECE
+            
+            if (tiempoEfecto > 20) {
+                active = false; //ELIMINARSE DESPUES DEL EFECTO
+            }
+        }
+    }
     
     @Override
     public void aplicarEfecto(Nave4 nave) {
@@ -17,13 +36,23 @@ public class VidaExtra extends GameObject implements Colisionable {
         return "VidaExtra";
     }
     
+
     @Override
     public boolean debeEliminarse() {
-        return true;
+        
+        return efectoActivado && tiempoEfecto > 20;
     }
     
     @Override
     public void onColision() {
-        active = false;
+        
+        efectoActivado = true;
+        activarEfectoVisual();
+    }
+    
+    @Override
+    public void activarEfectoVisual() {
+        //SE CAMBIA A COLOR DORADO POR UN MOMENTO
+        spr.setColor(1, 1, 0.3f, 1); // Color dorado
     }
 }
