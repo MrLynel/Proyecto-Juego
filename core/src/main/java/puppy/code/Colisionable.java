47ad95a6127038ae95efcc9a94d4f5a1d9ddd296
Alex Reyes <alex.reyes.o@mail.pucv.cl>
2025-10-31 21:05:22 -0300

    Create Colisionable.java
    Adicion de colisionable el cual servira para power ups

---
+++ core/src/main/java/puppy/code/Colisionable.java
@@ -0,0 +1,7 @@
+package puppy.code;
+
+public interface Colisionable {
+    boolean debeEliminarse();
+    void onColision();
+    void activarEfectoVisual(); //SE AGREGARAN EFECTO VISUALES PARA LOS OBJETOS (POWER UPS)
+}
